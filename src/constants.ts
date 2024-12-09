2be9fdda929eb3f4d41cded97afa2845e18492b6
opVOID <mannaushack@protonmail.com>
2024-12-09 15:15:37 +0100

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,7 @@ export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? 'https://mainne
 
 // Solana address that will receive fees when somebody plays on this platform
 export const PLATFORM_CREATOR_ADDRESS = new PublicKey(
-  'A35outJG52f54vJCT4uoSJndyHTAgc3V3jj4NgRTwtxu',
+  'BxgmQhBPTpsPpdLNXDj95eFSMAPwt3uEELnNZWmxqhLC',
 )
 
 // Gamba explorer URL - Appears in RecentPlays
